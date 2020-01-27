b846a2d2d3ae0d707bc0c9bfb9d4bf44e0ebb288
Alex Ghiurau <alexghiurau003@gmail.com>
2020-01-27 13:32:59 +0000

    start implementing register page

--- server.js
+++ server.js
@@ -22,28 +22,21 @@ app.use("/", (req, res, next) => {
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
+// Body parser
+app.use(express.urlencoded({ extended: true }));
+
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
-
-// Static files
-// app.use(
-//   "/",
-//   express.static("build", {
-//     extensions: ["html"]
-//   })
-// );
-
 
 // Declare port and start server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, err => {
   err
-    ? console.log("There was an error starting the server.", err)
+    ? console.log("Error starting the server.", err)
     : console.log(`Server running on port ${PORT}.`);
 });
-
 
 // For sending error codes
 function error(res, err) {
