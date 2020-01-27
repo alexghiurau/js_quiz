403363c7bb399e1611374990d1a27582e4ddf712
Alex Ghiurau <alexghiurau003@gmail.com>
2020-01-27 22:45:07 +0000

    prettier, finish boilerplate authentication method

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require("express");
-const expressLayouts = require('express-ejs-layouts');
-const mongoose = require('mongoose');
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
 
 const app = express();
 
+// Passport config
+require("./config/passport")(passport);
+
 // DB Configuration
-const db = require('./config/keys').MongoURI;
+const db = require("./config/keys").MongoURI;
 
 // Connect to MongoDB
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected.'))
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected."))
   .catch(err => console.log(err));
 
 // logging
@@ -20,14 +27,38 @@ app.use("/", (req, res, next) => {
 
 // EJS
 app.use(expressLayouts);
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Body parser
 app.use(express.urlencoded({ extended: true }));
 
+// Express session
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Globals
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
+
 // Routes
-app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
 
 // Declare port and start server
 const PORT = process.env.PORT || 5000;
