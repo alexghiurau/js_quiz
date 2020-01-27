be6a97272f1130123eb2acae03bbe9d9ee5a4df2
Alex Ghiurau <alexghiurau003@gmail.com>
2020-01-27 12:16:46 +0000

    fix mongo connection

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const db = require('./config/keys').MongoURI;
 
 // Connect to MongoDB
-mongoose.connect(db, { useNewUrlParser: true })
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected.'))
   .catch(err => console.log(err));
 
