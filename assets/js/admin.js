73cccfa02c73d266bc40d60f9e0ff739037f035d
Alex Ghiurau <alexghiurau003@gmail.com>
2020-02-16 13:59:31 +0000

    improve admin layout

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -7,16 +7,18 @@
 
   users.forEach(user => {
     output.push(
-      `<div class="card" style="width: 10rem;>
+      `
+      <div class="col-sm-3">
+      <div class="card">
             <div class="card-body">
               <h5 class="card-title">${user.name}</h5>
-              <p class="card-text">
+              <a class="card-text" href="mailto:${user.email}">
                   ${user.email}
               </p>
               <a href="#" class="btn btn-primary btn-users btn-sm">View Past Results</a>
               <a href="#" class="btn btn-warning btn-users btn-sm" data-userid="${user._id}" onClick=resetPersonality(event)>Reset Personality</a>
             </div>
-          </div>`
+          </div></div>`
     );
     usersContainer.innerHTML = output.join("");
   });
